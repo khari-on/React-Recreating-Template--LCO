7edcc75f22ed8c285c0e16ceecb5794a99256a6d
Hari <Khariprasath351@gmail.com>
2022-05-11 09:23:17 +0530

    nav bar created

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Nav from './nav'
+
+const App =()=>{
+    return(
+        <>
+        <Nav />
+        
+        </>
+        
+    )
+}
+
+ReactDOM.render(<App />,document.getElementById("root"))
